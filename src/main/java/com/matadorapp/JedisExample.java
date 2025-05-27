a840b7e5f0753d3202b3230c7628b14388e99e11
Patrick Guinard <patrick@public.com>
2025-05-27 11:11:46 -0600

    feat: Initial add

---
+++ src/main/java/com/matadorapp/JedisExample.java
@@ -0,0 +1,15 @@
+package com.matadorapp;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisExample {
+    public static void main(String[] args) {
+        try (Jedis jedis = new Jedis("localhost", 10044)) {
+            jedis.set("foo", "bar");
+            String value = jedis.get("foo");
+            System.out.println("Value from Redis: " + value);
+        } catch (Exception e) {
+            System.err.println("Could not connect to Redis: " + e.getMessage());
+        }
+    }
+}
