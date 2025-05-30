dfbd0c9b26177970629a623e62905d0ff0d8b4a9
Patrick Guinard <patrick@public.com>
2025-05-30 09:19:36 -0600

    feat: Add cluster command examples

--- src/main/java/com/matadorapp/JedisExample.java
+++ src/main/java/com/matadorapp/JedisExample.java
@@ -7,7 +7,9 @@ public class JedisExample {
         try (Jedis jedis = new Jedis("localhost", 10044)) {
             jedis.set("foo", "bar");
             String value = jedis.get("foo");
+            long keyslot = jedis.clusterKeySlot("a");
             System.out.println("Value from Redis: " + value);
+            System.out.println("Keys slot: " + keyslot);
         } catch (Exception e) {
             System.err.println("Could not connect to Redis: " + e.getMessage());
         }
